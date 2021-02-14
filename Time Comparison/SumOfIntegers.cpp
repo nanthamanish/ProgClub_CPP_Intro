afa259f08bbafb93c9ff493743e579f905dead2f
Manishnantha M <59362872+nanthamanish@users.noreply.github.com>
2021-02-14 11:19:14 +0530

    Create SumOfIntegers.cpp

---
+++ Time Comparison/SumOfIntegers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono;
+
+void printSum()
+{
+    long long s = 0;
+    for (int i = 0; i <= 1e6; i++)
+        s += i;
+    cout << s << '\n';
+}
+
+int main()
+{
+    auto start = high_resolution_clock::now();
+    printSum();
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken: " << duration.count() << " microseconds\n";
+
+    return 0;
+}
